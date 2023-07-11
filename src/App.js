17d26294c0a070bc992c2cbcb5d734d835a73953
Abhikant kumar <abhiparnav12@gmail.com>
2023-07-11 14:18:41 +0530

    added view course page and its components

--- src/App.js
+++ src/App.js
@@ -23,7 +23,11 @@ import AddCourse from "./components/core/Dashboard/AddCourse";
 import EditCourse from "./components/core/Dashboard/EditCourse";
 import MyCourses from "./components/core/Dashboard/MyCourses";
 import Catalog from "./pages/Catalog";
+import Error from "./pages/Error"
 import CourseDetails from "./pages/CourseDetails";
+import ViewCourse from "./pages/ViewCourse";
+import VideoDetails from "./components/core/ViewCourse/VideoDetails";
+//import Instructor from "./components/core/Dashboard/InstructorDashboard/Instructor";
 
 
 
@@ -42,45 +46,52 @@ function App() {
       <Navbar/>                                         {/* navbar is common in all pages */}
       <Routes>
         
-        <Route path="/" element={<Home/>} />
-        <Route path="catalog/:catalogName" element={<Catalog/>} />
-        <Route path="courses/:courseId" element={<CourseDetails/>} />
-        <Route path="signup" element = { <OpenRoute> <Signup /> </OpenRoute> } />
-        <Route path="login" element = { <OpenRoute> <Login /> </OpenRoute> } />
-        <Route path="forgot-password" element = { <OpenRoute> <ForgotPassword /> </OpenRoute> } />
-        <Route path="verify-email" element = { <OpenRoute> <VerifyEmail /> </OpenRoute> } />
-        <Route path="update-password/:id" element = { <OpenRoute> <UpdatePassword /> </OpenRoute> } />
-        <Route path="about" element = { <OpenRoute> <About /> </OpenRoute> } />
-        <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home/>} />
+          <Route path="catalog/:catalogName" element={<Catalog/>} />
+          <Route path="courses/:courseId" element={<CourseDetails/>} />
+          <Route path="signup" element = { <OpenRoute> <Signup /> </OpenRoute> } />
+          <Route path="login" element = { <OpenRoute> <Login /> </OpenRoute> } />
+          <Route path="forgot-password" element = { <OpenRoute> <ForgotPassword /> </OpenRoute> } />
+          <Route path="verify-email" element = { <OpenRoute> <VerifyEmail /> </OpenRoute> } />
+          <Route path="update-password/:id" element = { <OpenRoute> <UpdatePassword /> </OpenRoute> } />
+          <Route path="about" element = { <OpenRoute> <About /> </OpenRoute> } />
+          <Route path="/contact" element={<Contact />} />
 
-        <Route element = {<PrivateRoute> <Dashboard /> </PrivateRoute>} >
+          <Route element = {<PrivateRoute> <Dashboard /> </PrivateRoute>} >
 
-            <Route path="dashboard/my-profile" element={ < MyProfile />} /> 
-            <Route path="dashboard/Settings" element={<Settings />} /> 
+              <Route path="dashboard/my-profile" element={ < MyProfile />} /> 
+              <Route path="dashboard/Settings" element={<Settings />} /> 
 
-                {
-                    user?.accountType === ACCOUNT_TYPE.STUDENT && (
-              <>
-              <Route path="dashboard/cart" element={<Cart />} />
-              <Route path="dashboard/enrolled-courses" element={<EnrolledCourses />} />
-              </>
-            )
-          }
+                { user?.accountType === ACCOUNT_TYPE.STUDENT && (          
+                                    <>
+                                        <Route path="dashboard/cart" element={<Cart />} />
+                                        <Route path="dashboard/enrolled-courses" element={<EnrolledCourses />} />
+                                    </>
+                                )           
+                  }
 
-           {
-        user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
-          <>
-          {/* <Route path="dashboard/instructor" element={<Instructor />} /> */}
-          <Route path="dashboard/add-course" element={<AddCourse />} />
-          <Route path="dashboard/edit-course/:courseId" element={<EditCourse />} />
-          <Route path="dashboard/my-courses" element={<MyCourses />} />
+                { user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
+                                        <>
+                                            {/* <Route path="dashboard/instructor" element={<Instructor />} /> */}
+                                            <Route path="dashboard/add-course" element={<AddCourse />} />
+                                            <Route path="dashboard/edit-course/:courseId" element={<EditCourse />} />
+                                            <Route path="dashboard/my-courses" element={<MyCourses />} />    
+                                        </>
+                                  )
+                    }
+          </Route>
           
-          </>
-            )
-          }
+          <Route element = { <PrivateRoute> <ViewCourse /> </PrivateRoute> }>
+                { user?.accountType === ACCOUNT_TYPE.STUDENT && (
+                                                <>
+                                                  <Route path="view-course/:courseId/section/:sectionId/sub-section/:subSectionId" element={<VideoDetails />} /> 
+                                                </>
+                                  )
+                  }
+          </Route>
 
-        </Route>
-  
+          <Route path="*" element={<Error />} />
+
 
       </Routes>
 
--- src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
 import IconBtn from "../../../common/IconBtn"
-// import { buyCourse } from "../../../../services/operations/studentFeaturesAPI"
+import { buyCourse } from "../../../../services/operations/studentFeaturesAPI"
 
 export default function RenderTotalAmount() {
   const { total, cart } = useSelector((state) => state.cart)
@@ -13,7 +13,7 @@ export default function RenderTotalAmount() {
 
   const handleBuyCourse = () => {
     const courses = cart.map((course) => course._id)
-   // buyCourse(token, courses, user, navigate, dispatch)
+    buyCourse(token, courses, user, navigate, dispatch)
   }
 
   return (
